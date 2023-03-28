3b84ea3c8b3b4599d9e6b0f3d0724f52c213cb3a
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2023-03-28 15:58:59 +0200

    add methode Source

--- src/test/java/book/BookTest.java
+++ src/test/java/book/BookTest.java
@@ -1,6 +1,7 @@
 package book;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 
@@ -8,33 +9,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
 
-    @org.junit.jupiter.api.Test
+    @MethodSource
     void getTitre() {
         Book b1= new Book("le rouge et le noir", "victor hugo ", LocalDate.now());
         Assertions.assertEquals("le rouge et le noir",b1.getTitre());
     }
 
-    @org.junit.jupiter.api.Test
+    @MethodSource
     void setTitre() {
         Book b1= new Book("le rouge et le noir", "victor hugo ", LocalDate.now());
         b1.setTitre("les misérables");
         Assertions.assertEquals("les misérables",b1.getTitre());
     }
 
-    @org.junit.jupiter.api.Test
+    @MethodSource
     void getAuteur() {
         Book b1= new Book("le rouge et le noir", "victor hugo", LocalDate.now());
         Assertions.assertEquals("victor hugo",b1.getAuteur());
     }
 
-    @org.junit.jupiter.api.Test
+    @MethodSource
     void setAuteur() {
         Book b1= new Book("le rouge et le noir", "victor hugo", LocalDate.now());
         b1.setAuteur("mohamed");
         Assertions.assertEquals("mohamed",b1.getAuteur());
     }
 
-    @org.junit.jupiter.api.Test
+    @MethodSource
     void getDateDePub() {
         Book b1= new Book("le rouge et le noir", "victor hugo", LocalDate.now());
         Assertions.assertEquals(LocalDate.now(),b1.getDateDePub());
