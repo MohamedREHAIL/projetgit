17a5d2a85d7e16ddaca3936bc56c2ba5ddda858b
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2023-03-31 16:02:50 +0200

    deliveryTest

--- src/test/java/delivery/DeliveryTest.java
+++ src/test/java/delivery/DeliveryTest.java
@@ -10,7 +10,12 @@ class DeliveryTest {
     @Test
     void calculateCost() {
         Delivery d1=new Delivery(30,30);
-        double cout=d1.calculateCost();
-        Assertions.assertEquals(15.0,cout);
+        Delivery d2=new Delivery(70,30);
+        Delivery d3=new Delivery(10,15);
+        Assertions.assertEquals(15.0,d1.calculateCost());
+        Assertions.assertEquals(25.0,d2.calculateCost());
+        Assertions.assertEquals(12.0,d3.calculateCost());
+
+
     }
 }
